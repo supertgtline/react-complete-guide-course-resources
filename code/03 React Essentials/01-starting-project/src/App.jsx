059074532709a577a77d299c0a769bf4f1331657
debian <debian@debian.com>
2025-04-19 20:12:04 +0700

    Format Code

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -6,33 +6,33 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-const [selectedTopic, setSelectedTopic] = useState();
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedButton);
-};
-let tabContent = <p>Please select a topic.</p>
-if (selectedTopic) {
-  tabContent = <div id ="tab-content">
-  <h3>{EXAMPLES[selectedTopic].title}</h3>
-  <p>{EXAMPLES[selectedTopic].description}</p>
-  <pre>
-    <code>{EXAMPLES[selectedTopic].code}</code>
-  </pre>
-</div>
-}
+  };
+  let tabContent = <p>Please select a topic.</p>
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  }
 
-console.log('APP COMPNENT EXECUTING');
+  console.log('APP COMPNENT EXECUTING');
   return (
     <div>
       <Header />
       <main>
-        <section id = "core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          {CORE_CONCEPTS.map((coreConcept) => <CoreConcept key={coreConcept.title} {...coreConcept}></CoreConcept> )}
-        </ul>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((coreConcept) => <CoreConcept key={coreConcept.title} {...coreConcept}></CoreConcept>)}
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
@@ -40,7 +40,7 @@ console.log('APP COMPNENT EXECUTING');
             <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick('components')}>Components</TabButton>
             <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick('jsx')}>JSX</TabButton>
             <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick('props')}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onClick={() =>handleClick('state')}> State</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick('state')}> State</TabButton>
           </menu>
           {tabContent}
         </section>
