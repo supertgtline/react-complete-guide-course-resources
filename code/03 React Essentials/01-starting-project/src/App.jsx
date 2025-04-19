37390a3a7d1545e608f8ea0233ac32763731457c
debian <debian@debian.com>
2025-04-19 15:04:18 +0700

    58 Outputting list data dynamically

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -31,14 +31,7 @@ console.log('APP COMPNENT EXECUTING');
         <section id = "core-concepts">
         <h2>Core Concepts</h2>
         <ul>
-          <CoreConcept 
-          title={CORE_CONCEPTS[0].title}
-          description={CORE_CONCEPTS[0].description}
-          image={CORE_CONCEPTS[0].image}
-          />
-          <CoreConcept {...CORE_CONCEPTS[1]} />
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
+          {CORE_CONCEPTS.map((coreConcept) => <CoreConcept key={coreConcept.title} {...coreConcept}></CoreConcept> )}
         </ul>
         </section>
         <section id="examples">
