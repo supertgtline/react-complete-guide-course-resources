223df06e654e4f4024cb054d54fa5ee8a74183bd
debian <debian@debian.com>
2025-04-19 10:27:48 +0700

    55 Rendering content conditionally

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -6,12 +6,22 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-const [selectedTopic, setSelectedTopic] = useState('components');
+const [selectedTopic, setSelectedTopic] = useState();
 
   function handleClick(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedButton);
 };
+let tabContent = <p>Please select a topic.</p>
+if (selectedTopic) {
+  tabContent = <div id ="tab-content">
+  <h3>{EXAMPLES[selectedTopic].title}</h3>
+  <p>{EXAMPLES[selectedTopic].description}</p>
+  <pre>
+    <code>{EXAMPLES[selectedTopic].code}</code>
+  </pre>
+</div>
+}
 
 console.log('APP COMPNENT EXECUTING');
   return (
@@ -39,13 +49,7 @@ console.log('APP COMPNENT EXECUTING');
             <TabButton onClick={() => handleClick('props')}>Props</TabButton>
             <TabButton onClick={() =>handleClick('state')}> State</TabButton>
           </menu>
-          <div id ="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
